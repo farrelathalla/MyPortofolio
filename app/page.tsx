a99a990926adfd258977e1ce6d671cd456edb873
farrelathalla <farrelxag@gmail.com>
2024-08-06 18:19:56 +0700

    style ordered imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Hero from "@components/Hero.tsx";
 import { FloatingNav } from "@components/ui/floating-navbar";
 import { navItems } from "@data";
+import Hero from "@components/Hero.tsx";
 import MinatBakat from "@components/MinatBakat";
 import Projects from "@components/Projects";
 import Achievements from "@components/Achievements";
@@ -8,7 +8,7 @@ import Achievements from "@components/Achievements";
 const Home = () => {
   return (
     <main className="overflow-x-hidden">
-      <FloatingNav navItems={navItems} />
+      <FloatingNav navItems={navItems} /> 
       <Hero />
       <MinatBakat />
       <Projects />
